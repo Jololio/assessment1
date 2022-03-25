3e8da9195f842ea4c1b5e68d488d31b3313a4270
Joseph Al-Abudi <josephalabudi@gmail.com>
2022-03-25 10:05:56 -0600

    finished the sldc.js file

---
+++ sldc.js
@@ -0,0 +1,25 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Planning
+// Analysis of Requirements
+// Design
+// Testing & Integration
+// Implementation
+// Maintain
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added."
+const analysisOfRequirements = "This is the second step, in which the development team goes through all requirments of functionality and figures out what exactly does the software need to do." 
+const design = "This is the 3rd step, where the actual code of the software is written, typically in a system called pair programming."
+const testingAndIntegration = "This is the 4th step, once the software has been officially designed, the developers begin debugging the code with tests that make sure the code does what it is supposed to do, and integrate the code one the process is complete."
+const implementation = "This is the 5th step, once the code has been bugfixed and all requirements are met, then the developers must actually make the code go live for other people to use."
+const maintain = "This is the final step, once the code is built, bugfixed, and then goes live, it is still possible for the software to break or bugs to be missed. This requires the last step to maintain the code and make sure that it remains functional."
